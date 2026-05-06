36971ac3475ee6e47d1db833070d2d8452a27adc
Siddarth <siddarth1099@gmail.com>
2026-05-06 22:11:25 +0530

    adding getters and setters

--- src/src/Log.java
+++ src/src/Log.java
@@ -8,4 +8,28 @@ public class Log {
         this.message = message;
         this.term = term;
     }
+
+    public int getMessageID() {''
+        return messageID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getTerm() {
+        return term;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setMessageID(int messageID) {
+        this.messageID = messageID;
+    }
 }
